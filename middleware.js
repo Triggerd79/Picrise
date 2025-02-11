c29d56a47408e61823dbb0383c72b29891d20404
Triggerd79 <parthkharbanda.pk776@gmail.com>
2025-02-12 01:45:26 +0530

    Remove '/explore' from public route matcher in middleware

--- middleware.js
+++ middleware.js
@@ -2,7 +2,6 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
   '/',
-  '/explore',
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/api/webhook/clerk(.*)',
